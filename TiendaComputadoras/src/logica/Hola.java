3b4547fce300ea82aa5960ae1dabb0c0ed41a311
Oliver <Oliver@OliverPC>
2019-10-31 15:24:21 -0400

    creación, gets y sets de la variable prueba.

--- TiendaComputadoras/src/logica/Hola.java
+++ TiendaComputadoras/src/logica/Hola.java
@@ -1,5 +1,14 @@
 package logica;
 
 public class Hola {
+	private String prueba;
+
+	public String getPrueba() {
+		return prueba;
+	}
+
+	public void setPrueba(String prueba) {
+		this.prueba = prueba;
+	}
 
 }
