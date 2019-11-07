e4c877b9ee2013253aa70b93a2bc5c7747ff647b
gusgo <gusgo@LAPTOP-TPPBSJFQ>
2019-11-07 14:43:58 -0800

    Cree Clases Cliente, Persona y Proveedor (Con constructores, sets y gets). Borre clase Hola

---
+++ TiendaComputadoras/src/logica/Proveedor.java
@@ -0,0 +1,36 @@
+package logica;
+
+import java.util.ArrayList;
+
+public class Proveedor extends Persona {
+	private ArrayList<Componente> misCompos;
+	private ArrayList<OrdenCompra> misOrdenes;
+	private float debito;
+	public Proveedor(String nombre, String telefono, String direccion, ArrayList<Componente> misCompos,
+			ArrayList<OrdenCompra> misOrdenes, float debito) {
+		super(nombre, telefono, direccion);
+		this.misCompos = misCompos;
+		this.misOrdenes = misOrdenes;
+		this.debito = debito;
+	}
+	public ArrayList<Componente> getMisCompos() {
+		return misCompos;
+	}
+	public void setMisCompos(ArrayList<Componente> misCompos) {
+		this.misCompos = misCompos;
+	}
+	public ArrayList<OrdenCompra> getMisOrdenes() {
+		return misOrdenes;
+	}
+	public void setMisOrdenes(ArrayList<OrdenCompra> misOrdenes) {
+		this.misOrdenes = misOrdenes;
+	}
+	public float getDebito() {
+		return debito;
+	}
+	public void setDebito(float debito) {
+		this.debito = debito;
+	}
+	
+
+}
