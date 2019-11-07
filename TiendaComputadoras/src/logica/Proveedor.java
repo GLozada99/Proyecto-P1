0dbde89741c10a3ec2fe1f414ca07cd38838a3e2
gusgo <gusgo@LAPTOP-TPPBSJFQ>
2019-11-07 15:54:52 -0800

    Lo de ahorita de los proveedores

--- TiendaComputadoras/src/logica/Proveedor.java
+++ TiendaComputadoras/src/logica/Proveedor.java
@@ -7,12 +7,12 @@ public class Proveedor extends Persona {
 	private ArrayList<OrdenCompra> misOrdenes;
 	private float debito;
 	private ArrayList<Float> preciosCompos;
-	public Proveedor(String nombre, String telefono, String direccion, ArrayList<Componente> misCompos,
-			ArrayList<OrdenCompra> misOrdenes, float debito) {
+	public Proveedor(String nombre, String telefono, String direccion, ArrayList<Componente> misCompos,ArrayList<Float> preciosCompos) {
 		super(nombre, telefono, direccion);
-		this.misCompos = new ArrayList<>();
-		this.misOrdenes = misOrdenes;
-		this.debito = debito;
+		this.misCompos = misCompos;
+		this.misOrdenes = new ArrayList<>();
+		this.setPreciosCompos(preciosCompos);
+		debito = 0;
 	}
 	public ArrayList<Componente> getMisCompos() {
 		return misCompos;
@@ -32,6 +32,14 @@ public class Proveedor extends Persona {
 	public void setDebito(float debito) {
 		this.debito = debito;
 	}
-	
+	public ArrayList<Float> getPreciosCompos() {
+		return preciosCompos;
+	}
+	public void setPreciosCompos(ArrayList<Float> preciosCompos) {
+		this.preciosCompos = preciosCompos;
+	}
+	public float getPrecioCompo(int i) {
+		return preciosCompos.lastIndexOf(misCompos.get(i));
+	}
 
 }
