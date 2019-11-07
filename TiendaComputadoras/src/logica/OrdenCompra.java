99b57ccd3852771400d61593651fcc0f69d450ad
gusgo <gusgo@LAPTOP-TPPBSJFQ>
2019-11-07 14:56:22 -0800

    Los SyG de OrdenCompra

--- TiendaComputadoras/src/logica/OrdenCompra.java
+++ TiendaComputadoras/src/logica/OrdenCompra.java
@@ -10,5 +10,23 @@ public class OrdenCompra {
 		this.cantiCompos = cantiCompos;
 		this.realizada = realizada;
 	}
+	public Componente getCompCompra() {
+		return compCompra;
+	}
+	public void setCompCompra(Componente compCompra) {
+		this.compCompra = compCompra;
+	}
+	public int getCantiCompos() {
+		return cantiCompos;
+	}
+	public void setCantiCompos(int cantiCompos) {
+		this.cantiCompos = cantiCompos;
+	}
+	public boolean isRealizada() {
+		return realizada;
+	}
+	public void setRealizada(boolean realizada) {
+		this.realizada = realizada;
+	}
 
 }
