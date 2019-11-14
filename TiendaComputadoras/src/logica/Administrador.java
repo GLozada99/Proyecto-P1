8f3fb55b162303c1b8cf1635036d1c189083a7e0
gusgo <gusgo@LAPTOP-TPPBSJFQ>
2019-11-13 18:13:19 -0800

    Cree la clase Administrador

---
+++ TiendaComputadoras/src/logica/Administrador.java
@@ -0,0 +1,22 @@
+package logica;
+
+public class Administrador extends Persona {
+	private String contraseña;
+	
+	public Administrador(String nombre, String telefono, String direccion, String cedula, String contraseña) {
+		super(nombre, telefono, direccion, cedula);
+		this.contraseña=contraseña;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	
+	
+
+}
