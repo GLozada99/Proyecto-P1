40089172fe193f98f3c2c8de470c9e487670c6fc
gusgo <gusgo@LAPTOP-TPPBSJFQ>
2019-11-13 18:05:18 -0800

    Cree un main

--- TiendaComputadoras/src/logica/Main.java
+++ TiendaComputadoras/src/logica/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Persona c1=new Cliente("Juan", "00", "", "11");
 		Tienda.getInstance().getLosClientes().add((Cliente) c1);
-		
+		//Componente co1=new DiscoDuro(modelo, numeroSerie, marca, cantDisponible, cantMin, cantMax, precioVentaI, precioCompraI, capacidadAlma, tipoConexion);
 		System.out.println(Tienda.getInstance().findClientebyCedula("11").getNombre());
 
 	}
