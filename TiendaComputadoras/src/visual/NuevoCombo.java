57c715800612039d59a0dd3fb59ce3a95678bb6a
ALONDRA DOMINGUEZ <ALONDRA DOMINGUEZ@LAPTOP-0UR900CT>
2019-11-21 12:27:58 -0400

    En Nuevo Combo se crearon 4 métodos para que se carguen los componentes al momento que el administrador vaya a escoger los elementos que formarán parte de un nuevo combo. Se creó esta ventana para registrar los combos nuevos.

--- TiendaComputadoras/src/visual/NuevoCombo.java
+++ TiendaComputadoras/src/visual/NuevoCombo.java
@@ -37,7 +37,12 @@ public class NuevoCombo extends JDialog {
 	private static DefaultTableModel tableModel;
 	private static Object[] fila;
 	private static JTable table;
-	
+	private JComboBox cbxDiscoDuro;
+	private JComboBox cbxMicro;
+	private JComboBox cbxRAM;
+	private JComboBox cbxMotherBoard;
+
+
 
 	/**
 	 * Launch the application.
@@ -62,90 +67,90 @@ public class NuevoCombo extends JDialog {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(null, "Datos del Combo", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel.setBounds(5, 5, 496, 85);
 		contentPanel.add(panel);
 		panel.setLayout(null);
-		
+
 		JLabel lblCdigo = new JLabel("C\u00F3digo:");
 		lblCdigo.setBounds(10, 25, 46, 14);
 		panel.add(lblCdigo);
-		
+
 		JLabel lblPrecio = new JLabel("Precio:");
 		lblPrecio.setBounds(10, 50, 46, 14);
 		panel.add(lblPrecio);
-		
+
 		JLabel lblCantidad = new JLabel("Cantidad de Combos:");
 		lblCantidad.setBounds(231, 25, 140, 14);
 		panel.add(lblCantidad);
-		
+
 		JLabel lblPorcentajeDeDescuento = new JLabel("Porcentaje de descuento: ");
 		lblPorcentajeDeDescuento.setBounds(231, 50, 158, 14);
 		panel.add(lblPorcentajeDeDescuento);
-		
+
 		JSpinner spinner = new JSpinner();
 		spinner.setModel(new SpinnerNumberModel(new Integer(1), new Integer(1), null, new Integer(1)));
 		spinner.setBounds(399, 22, 44, 20);
 		panel.add(spinner);
-		
+
 		JSpinner spinner_1 = new JSpinner();
 		spinner_1.setModel(new SpinnerNumberModel(1, 1, 100, 1));
 		spinner_1.setBounds(399, 47, 44, 20);
 		panel.add(spinner_1);
-		
+
 		textField = new JTextField();
 		textField.setBounds(63, 22, 86, 20);
 		panel.add(textField);
 		textField.setColumns(10);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setBounds(63, 47, 86, 20);
 		panel.add(textField_1);
 		textField_1.setColumns(10);
-		
+
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new TitledBorder(null, "Componentes del Combo", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel_1.setBounds(5, 91, 541, 211);
 		contentPanel.add(panel_1);
 		panel_1.setLayout(null);
-		
+
 		JLabel lblDiscoDuro = new JLabel("Disco Duro:");
 		lblDiscoDuro.setBounds(13, 34, 99, 14);
 		panel_1.add(lblDiscoDuro);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"C\u00F3digo   :    Marca    :    Modelo   :   Cantidad    :     Capacidad   "}));
-		comboBox.setBounds(122, 31, 371, 20);
-		panel_1.add(comboBox);
-		
+
+		cbxDiscoDuro = new JComboBox();
+		cbxDiscoDuro.setModel(new DefaultComboBoxModel(new String[] {"C\u00F3digo   :    Marca    :    Modelo   :   Cantidad    :     Capacidad   "}));
+		cbxDiscoDuro.setBounds(122, 31, 371, 20);
+		panel_1.add(cbxDiscoDuro);
+
 		JLabel lblMemoriaRam = new JLabel("Memoria RAM:");
 		lblMemoriaRam.setBounds(10, 121, 102, 14);
 		panel_1.add(lblMemoriaRam);
-		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"C\u00F3digo   :    Marca    :    Modelo   :     Cantidad   :    Velocidad"}));
-		comboBox_1.setBounds(122, 77, 371, 20);
-		panel_1.add(comboBox_1);
-		
+
+		cbxMicro = new JComboBox();
+		cbxMicro.setModel(new DefaultComboBoxModel(new String[] {"C\u00F3digo   :    Marca    :    Modelo   :     Cantidad   :    Velocidad"}));
+		cbxMicro.setBounds(122, 77, 371, 20);
+		panel_1.add(cbxMicro);
+
 		JLabel lblMicroprocesador = new JLabel("Microprocesador:");
 		lblMicroprocesador.setBounds(10, 80, 102, 14);
 		panel_1.add(lblMicroprocesador);
-		
-		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"C\u00F3digo   :    Marca    :    Modelo   :     Cantidad   :    Memoria"}));
-		comboBox_2.setBounds(122, 123, 371, 20);
-		panel_1.add(comboBox_2);
-		
+
+		cbxRAM = new JComboBox();
+		cbxRAM.setModel(new DefaultComboBoxModel(new String[] {"C\u00F3digo   :    Marca    :    Modelo   :     Cantidad   :    Memoria"}));
+		cbxRAM.setBounds(122, 123, 371, 20);
+		panel_1.add(cbxRAM);
+
 		JLabel lblTarjetaMadre = new JLabel("Tarjeta Madre:");
 		lblTarjetaMadre.setBounds(10, 172, 102, 14);
 		panel_1.add(lblTarjetaMadre);
-		
-		JComboBox comboBox_3 = new JComboBox();
-		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"C\u00F3digo   :    Marca    :    Modelo   :     Cantidad   :    Tipo RAM"}));
-		comboBox_3.setBounds(122, 169, 371, 20);
-		panel_1.add(comboBox_3);
+
+		cbxMotherBoard = new JComboBox();
+		cbxMotherBoard.setModel(new DefaultComboBoxModel(new String[] {"C\u00F3digo   :    Marca    :    Modelo   :     Cantidad   :    Tipo RAM"}));
+		cbxMotherBoard.setBounds(122, 169, 371, 20);
+		panel_1.add(cbxMotherBoard);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -154,7 +159,7 @@ public class NuevoCombo extends JDialog {
 				JButton btnCrear = new JButton("Crear");
 				btnCrear.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						
+
 					}
 				});
 				btnCrear.setActionCommand("OK");
@@ -171,10 +176,53 @@ public class NuevoCombo extends JDialog {
 				btnCancelar.setActionCommand("Cancel");
 				buttonPane.add(btnCancelar);
 			}
+
+
 		}
+	}		
+	private void DiscoDuroCbx(){
+		cbxDiscoDuro.removeAllItems();
+	
+		for (Componente elComponente : Tienda.getInstance().getLosComponentes() ) {
+			if(elComponente instanceof DiscoDuro) {
+				cbxDiscoDuro.addItem(new String(elComponente.getNumeroSerie()+" : "+elComponente.getMarca()+" : "+elComponente.getModelo()+" : "+elComponente.getCantDisponible()+" : "+((DiscoDuro)elComponente).getCapacidadAlma()));
+			}}
+		//cbxDiscoDuro.insertItemAt(new String("<>"),0);
+		cbxDiscoDuro.setSelectedIndex(0);
+	}
+	private void MicroCbx(){
+		cbxMicro.removeAllItems();
+		
+		for (Componente elComponente : Tienda.getInstance().getLosComponentes() ) {
+			if(elComponente instanceof Micro) {
+				cbxMicro.addItem(new String(elComponente.getNumeroSerie()+" : "+elComponente.getMarca()+" : "+elComponente.getModelo()+" : "+elComponente.getCantDisponible()+" : "+((Micro)elComponente).getVelocidad()));
+			}}
+	//	cbxMicro.insertItemAt(new String("<Escoja una Cuenta>"),0);
+		cbxMicro.setSelectedIndex(0);
+	}
+	private void RAMCbx(){
+		cbxRAM.removeAllItems();
+		
+		for (Componente elComponente : Tienda.getInstance().getLosComponentes() ) {
+			if(elComponente instanceof RAM) {
+				cbxRAM.addItem(new String(elComponente.getNumeroSerie()+" : "+elComponente.getMarca()+" : "+elComponente.getModelo()+" : "+elComponente.getCantDisponible()+" : "+((RAM)elComponente).getCantMemoria()));
+			}}
+	//	cbxMicro.insertItemAt(new String("<Escoja una Cuenta>"),0);
+		cbxRAM.setSelectedIndex(0);
+	}
+	private void MotherBoardCbx(){
+		cbxMotherBoard.removeAllItems();
+		
+		for (Componente elComponente : Tienda.getInstance().getLosComponentes() ) {
+			if(elComponente instanceof MotherBoard) {
+				cbxMotherBoard.addItem(new String(elComponente.getNumeroSerie()+" : "+elComponente.getMarca()+" : "+elComponente.getModelo()+" : "+elComponente.getCantDisponible()+" : "+((MotherBoard)elComponente).getTipoRAM()));
+			}}
+	//	cbxMicro.insertItemAt(new String("<Escoja una Cuenta>"),0);
+		cbxMotherBoard.setSelectedIndex(0);
 	}
 	
-	public static void loadSportMans(int selection) {
+
+	/*public static void loadSportMans(int selection) {
 	tableModel.setRowCount(0);
 	fila = new Object[tableModel.getColumnCount()];
 	switch (selection) {
@@ -192,7 +240,7 @@ public class NuevoCombo extends JDialog {
 				fila[4] = "Memoria RAM";
 			if(aux instanceof MotherBoard)
 				fila[4] = "Tarjeta Madre";
-			
+
 			tableModel.addRow(fila);
 		}
 		break;
@@ -205,7 +253,7 @@ public class NuevoCombo extends JDialog {
 				fila[3] = aux.getCantDisponible();
 				fila[4]=((DiscoDuro) aux).getCapacidadAlma();//sera?
 			fila[5] = "Disco Duro";
-			
+
 			tableModel.addRow(fila);
 			}
 		}
@@ -219,7 +267,7 @@ public class NuevoCombo extends JDialog {
 				fila[3] = aux.getCantDisponible();
 				fila[4]= ((Micro) aux).getVelocidad();//ojo
 			fila[5] = "Microprocesador";
-			
+
 			tableModel.addRow(fila);
 			}
 		}
@@ -233,12 +281,12 @@ public class NuevoCombo extends JDialog {
 				fila[3] = aux.getCantDisponible();
 				fila[4]=((RAM) aux).getCantMemoria();
 			fila[5] = "Memoria RAM";
-			
+
 			tableModel.addRow(fila);
 			}
 		}
 		break;	
-		
+
 	case 4:
 		for (Componente aux : Tienda.getInstance().getLosComponentes()) {
 			if(aux instanceof MotherBoard){
@@ -248,14 +296,14 @@ public class NuevoCombo extends JDialog {
 				fila[3] = aux.getCantDisponible();
 				fila[4]=((MotherBoard) aux).getTipoRAM();
 			fila[5] = "Tarjeta Madre";
-			
+
 			tableModel.addRow(fila);
 			}
 		}
 		break;
 	}
-	
-	
+
+
 	table.setModel(tableModel);
 	table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 	table.getTableHeader().setReorderingAllowed(false);
@@ -265,6 +313,7 @@ public class NuevoCombo extends JDialog {
 	columnModel.getColumn(2).setPreferredWidth(150);
 	columnModel.getColumn(3).setPreferredWidth(130);
 	columnModel.getColumn(4).setPreferredWidth(81);
-	
-	}
+
+	}*/
+
 }
