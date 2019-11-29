65623a92b6b83575ed5c91f0e3f25fef4378c024
Oliver <Oliver@OliverPC>
2019-11-28 20:13:12 -0400

    Creación de la lista de usuarios y bloqueo de las algunas ventanas no accedibles por el usuario

---
+++ TiendaComputadoras/src/visual/ListaUsuarios.java
@@ -0,0 +1,167 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+
+import logica.Administrador;
+import logica.Cliente;
+import logica.Persona;
+import logica.Tienda;
+
+public class ListaUsuarios extends JDialog {
+	private final JPanel contentPanel = new JPanel();
+	private JTable table;
+	private JButton btnAceptar;
+	private static DefaultTableModel model;
+	private static Object[] row;
+	private JButton btnModificar;
+	private JButton btnEliminar;
+	private String codigo="";
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ListaClientes dialog = new ListaClientes();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+
+
+
+
+	public ListaUsuarios() {
+		setTitle("Lista Usuarios");
+
+		setResizable(false);
+		setBounds(100, 100, 582, 353);
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JPanel panel = new JPanel();
+			contentPanel.add(panel, BorderLayout.CENTER);
+			panel.setLayout(new BorderLayout(0, 0));
+			{
+				JScrollPane scrollPane = new JScrollPane();
+				scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+				panel.add(scrollPane, BorderLayout.CENTER);
+				{
+
+					model = new DefaultTableModel();
+					String[] header = {"Codigo","Nombre","Telefono","Direccion","Contraseña", "Tipo"};
+					model.setColumnIdentifiers(header);
+					table = new JTable();
+					table.addMouseListener(new MouseAdapter() {
+						@Override
+						public void mouseClicked(MouseEvent e) {
+							if(table.getSelectedRow()>-1&&Tienda.getInstance().getUsuarioActual() instanceof Administrador){
+								int index = table.getSelectedRow();
+								btnEliminar.setEnabled(true);
+								btnModificar.setEnabled(true);
+								codigo = String.valueOf(table.getValueAt(index, 0));
+
+
+							}
+						}
+					});
+					table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+					table.setModel(model);
+					scrollPane.setViewportView(table);
+				}
+			}
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				btnAceptar = new JButton("Aceptar");
+				btnAceptar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				buttonPane.add(btnAceptar);
+		
+				btnModificar = new JButton("Modificar");
+				btnModificar.setEnabled(false);
+				btnModificar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						ModificarCliente aux = new ModificarCliente(Tienda.getInstance().findClientebyCedula(codigo));
+						aux.setModal(true);
+						aux.setVisible(true);
+						cargarClientes();
+						/*dispose();
+						ListaClientes refresh = new ListaClientes();
+						refresh.setModal(true);
+						refresh.setVisible(true);*/
+					}
+				});
+				buttonPane.add(btnModificar);
+				
+				btnEliminar = new JButton("Eliminar");
+				btnEliminar.setEnabled(false);
+				btnEliminar.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+/*						Usuario aux = Tienda.getInstance().findClientebyCedula(codigo);
+						Tienda.getInstance().getLosClientes().remove(aux);
+						cargarClientes();
+						*/
+					}
+				});
+				buttonPane.add(btnEliminar);
+			}
+		}
+		cargarClientes();
+	}
+	{
+
+	}
+	public static void cargarClientes() {
+		model.setRowCount(0);
+		row = new Object[model.getColumnCount()];
+
+		
+		for (Persona aux : Tienda.getInstance().getLosUsuarios()) {
+			row[0] = aux.getCodigo();
+			row[1] = aux.getNombre();
+			row[2] = aux.getTelefono();
+			row[3] = aux.getDireccion();
+			row[4] = ((Administrador)aux).getContraseña();
+			if (aux instanceof Administrador) {
+				row[5] = "Administrador";
+			}else {
+				row[5] = "Vendedor";
+			} 
+			model.addRow(row);
+		}
+
+	}
+}
--- TiendaComputadoras/src/visual/Principal.java
+++ TiendaComputadoras/src/visual/Principal.java
@@ -20,6 +20,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import logica.Administrador;
 import logica.Cliente;
 import logica.Combo;
 import logica.Componente;
@@ -64,7 +65,7 @@ public class Principal extends JFrame {
 	 */
 	public Principal() {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Principal.class.getResource("/Imagenes/IconPrincipal.png")));
-	
+
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
@@ -81,13 +82,13 @@ public class Principal extends JFrame {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
+
 			}
 		});
 		//Cosas para probar
 		/*Persona ho = new Cliente("Juan", "809", "Aqui", "402-1383575-0");
 		Tienda.getInstance().getLosClientes().add((Cliente) ho);
-		
+
 		Persona ho1 = new Cliente("Juanaa", "809", "Aqui", "402-1111111-0");
 		Tienda.getInstance().getLosClientes().add((Cliente) ho1);
 		ArrayList<Componente> aux = new ArrayList<>();
@@ -96,24 +97,24 @@ public class Principal extends JFrame {
 		aux2.add((float) 350);
 		aux2.add((float) 450);
 		aux2.add((float) 550);
-		
+
 		Componente dd = new DiscoDuro("CMP-"+Tienda.getInstance().getGeneradorCodigoComponentes(), "Seagate", "XS", 3, 10, 1000, "2TB", "2");
 		aux.add(dd);Tienda.getInstance().agregarComponente(dd);Tienda.getInstance().primeraOrdenCompra(dd);dd.setCantDisponible(5);
-		
+
 		Componente ram = new RAM("CMP-"+Tienda.getInstance().getGeneradorCodigoComponentes(), "Dell", "XS", 1, 13, 800, "No se", "6");
 		aux.add(ram);Tienda.getInstance().agregarComponente(ram);Tienda.getInstance().primeraOrdenCompra(ram);ram.setCantDisponible(2);
-		
+
 		Componente micro = new Micro("CMP-"+Tienda.getInstance().getGeneradorCodigoComponentes(), "Ras", "XS", 4, 16, 750,"Se menos", "3");
 		aux.add(micro);Tienda.getInstance().agregarComponente(micro);Tienda.getInstance().primeraOrdenCompra(micro);micro.setCantDisponible(51);
-		
+
 		Componente mb = new MotherBoard("CMP-"+Tienda.getInstance().getGeneradorCodigoComponentes(), "HP", "XS", 6, 12, 1800,"Nada", "5");
 		aux.add(mb);Tienda.getInstance().agregarComponente(mb);Tienda.getInstance().primeraOrdenCompra(mb);mb.setCantDisponible(3);
-		
+
 	//	OrdenCompra p = new OrdenCompra("OC-"+Tienda.getInstance().getGeneradorCodigoOrdenCompra(),dd, 6);
-		
+
 	//	Combo comb = new Combo(aux, "Combo3", Tienda.getInstance().precioTotalComponentes(aux), 25, 2);
 		//Tienda.getInstance().agregarCombo(comb);
-		
+
 		Persona pr1 = new Proveedor("Juan", "0192", "Alla", "12123", aux, aux2);
 		Tienda.getInstance().getLosProveedores().add((Proveedor) pr1);*/
 		/*Tienda.getInstance().getLasFacturas().clear();
@@ -126,7 +127,7 @@ public class Principal extends JFrame {
 		Tienda.getInstance().getOrdenesSinProcesar().clear();*/
 		//Tienda.getInstance().getOrdenesSinProcesar().add(p);
 		//Termina
-		
+
 		dim = super.getToolkit().getScreenSize(); 
 		super.setSize(dim.width, dim.height);//-100);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -137,13 +138,14 @@ public class Principal extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
+
 		JMenu mnAdministracion = new JMenu("Administracion");
+		mnAdministracion.setEnabled(false);
 		menuBar.add(mnAdministracion);
-		
+
 		JMenuItem mntmAgregarComponentes = new JMenuItem("Agregar Componentes");
 		mntmAgregarComponentes.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconAgregarComponente.png")));
 		mntmAgregarComponentes.addActionListener(new ActionListener() {
@@ -154,54 +156,55 @@ public class Principal extends JFrame {
 			}
 		});
 		mnAdministracion.add(mntmAgregarComponentes);
-		
-				
-				JMenuItem mntmAgregarProveedores = new JMenuItem("Agregar  Proveedores");
-				mntmAgregarProveedores.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconAdmin.png")));
-				mntmAgregarProveedores.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						AgregarProveedor aux = new AgregarProveedor();
-						aux.setModal(true);
-						aux.setVisible(true);
-					}
-				});
-				mnAdministracion.add(mntmAgregarProveedores);
-				
-				JMenuItem mntmNuevoCombo = new JMenuItem("Nuevo Combo");
-				mntmNuevoCombo.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconAgregarCombopng.png")));
-				mntmNuevoCombo.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						NuevoCombo aux = new NuevoCombo();
-						aux.setModal(true);
-						aux.setVisible(true);
-					}
-				});
-				mnAdministracion.add(mntmNuevoCombo);
-				
-				JMenuItem mntmNuevoUsuario = new JMenuItem("Nuevo Usuario");
-				mntmNuevoUsuario.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						NuevoUsuario aux = new NuevoUsuario();
-						aux.setModal(true);
-						aux.setVisible(true);
-					}
-				});
-				mnAdministracion.add(mntmNuevoUsuario);
-				
-				JMenuItem mntmAsignarOrdenDe = new JMenuItem("Asignar orden de compra");
-				mntmAsignarOrdenDe.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconOrdenesCompra.png")));
-				mnAdministracion.add(mntmAsignarOrdenDe);
-				mntmAsignarOrdenDe.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						OrdenesPorProcesar aux = new OrdenesPorProcesar();
-						aux.setModal(true);
-						aux.setVisible(true);
-					}
-				});
-		
+
+
+		JMenuItem mntmAgregarProveedores = new JMenuItem("Agregar  Proveedores");
+		mntmAgregarProveedores.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconAdmin.png")));
+		mntmAgregarProveedores.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AgregarProveedor aux = new AgregarProveedor();
+				aux.setModal(true);
+				aux.setVisible(true);
+			}
+		});
+		mnAdministracion.add(mntmAgregarProveedores);
+
+		JMenuItem mntmNuevoCombo = new JMenuItem("Nuevo Combo");
+		mntmNuevoCombo.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconAgregarCombopng.png")));
+		mntmNuevoCombo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				NuevoCombo aux = new NuevoCombo();
+				aux.setModal(true);
+				aux.setVisible(true);
+			}
+		});
+
+		JMenuItem mntmNuevoUsuario = new JMenuItem("Nuevo Usuario");
+		mntmNuevoUsuario.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconUsuario.png")));
+		mntmNuevoUsuario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				NuevoUsuario aux = new NuevoUsuario();
+				aux.setModal(true);
+				aux.setVisible(true);
+			}
+		});
+		mnAdministracion.add(mntmNuevoUsuario);
+		mnAdministracion.add(mntmNuevoCombo);
+
+		JMenuItem mntmAsignarOrdenDe = new JMenuItem("Asignar orden de compra");
+		mntmAsignarOrdenDe.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconOrdenesCompra.png")));
+		mnAdministracion.add(mntmAsignarOrdenDe);
+		mntmAsignarOrdenDe.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				OrdenesPorProcesar aux = new OrdenesPorProcesar();
+				aux.setModal(true);
+				aux.setVisible(true);
+			}
+		});
+
 		JMenu mnFacturacion = new JMenu("Facturaci\u00F3n");
 		menuBar.add(mnFacturacion);
-		
+
 		JMenuItem mntmNuevaFactura = new JMenuItem("Nueva Factura");
 		mntmNuevaFactura.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconFacturar.png")));
 		mntmNuevaFactura.addActionListener(new ActionListener() {
@@ -212,7 +215,7 @@ public class Principal extends JFrame {
 			}
 		});
 		mnFacturacion.add(mntmNuevaFactura);
-		
+
 		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Abonar/Saldar Credito Cliente");
 		mntmNewMenuItem_1.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconAbonarSaldo.png")));
 		mntmNewMenuItem_1.addActionListener(new ActionListener() {
@@ -223,10 +226,10 @@ public class Principal extends JFrame {
 			}
 		});
 		mnFacturacion.add(mntmNewMenuItem_1);
-		
+
 		JMenu mnListados = new JMenu("Listados");
 		menuBar.add(mnListados);
-		
+
 		JMenuItem mntmListaClientes = new JMenuItem("Lista Clientes");
 		mntmListaClientes.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -235,9 +238,23 @@ public class Principal extends JFrame {
 				aux.setVisible(true);
 			}
 		});
-		mnListados.add(mntmListaClientes);
 		
+				JMenuItem mntmListaUsuarios = new JMenuItem("Lista Usuarios");
+				mntmListaUsuarios.setEnabled(false);
+				mntmListaUsuarios.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						ListaUsuarios aux = new ListaUsuarios();
+						aux.setModal(true);
+						aux.setVisible(true);
+					}
+				});
+				
+				mntmListaUsuarios.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconListaUsuario.png")));
+				mnListados.add(mntmListaUsuarios);
+		mnListados.add(mntmListaClientes);
+
 		JMenuItem mntmListaProveedores = new JMenuItem("Lista Proveedores");
+		mntmListaProveedores.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconListaProveedor.png")));
 		mntmListaProveedores.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ListaProveedores aux = new ListaProveedores(false);
@@ -245,9 +262,15 @@ public class Principal extends JFrame {
 				aux.setVisible(true);
 			}
 		});
+		if(Tienda.getInstance().getUsuarioActual() instanceof Administrador) {			
+			mnAdministracion.setEnabled(true);
+			mntmListaUsuarios.setEnabled(true);
+			
+		}
 		mnListados.add(mntmListaProveedores);
-		
+
 		JMenuItem mntmListaComponentes = new JMenuItem("Lista Componentes");
+		mntmListaComponentes.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconListaComponentes.png")));
 		mntmListaComponentes.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				ListaComponentes aux = new ListaComponentes(false);
@@ -256,8 +279,9 @@ public class Principal extends JFrame {
 			}
 		});
 		mnListados.add(mntmListaComponentes);
-		
+
 		JMenuItem mntmListaCombos = new JMenuItem("Lista Combos");
+		mntmListaCombos.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconListaCombos.png")));
 		mntmListaCombos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ListaCombos aux = new ListaCombos();
@@ -266,8 +290,9 @@ public class Principal extends JFrame {
 			}
 		});
 		mnListados.add(mntmListaCombos);
-		
+
 		JMenuItem mntmHistorialFacturas = new JMenuItem("Historial Facturas");
+		mntmHistorialFacturas.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconListaFacturar.png")));
 		mntmHistorialFacturas.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				HistorialFacturas aux = new HistorialFacturas();
@@ -276,8 +301,9 @@ public class Principal extends JFrame {
 			}
 		});
 		mnListados.add(mntmHistorialFacturas);
-		
+
 		JMenuItem mntmHistorialOrdenes = new JMenuItem("Historial Ordenes de Compra");
+		mntmHistorialOrdenes.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconListaOrdenCompra.png")));
 		mntmHistorialOrdenes.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				HistorialOrdenes aux = new HistorialOrdenes();
@@ -288,7 +314,7 @@ public class Principal extends JFrame {
 		mnListados.add(mntmHistorialOrdenes);
 		getContentPane().setLayout(null);
 	}
-		
-	}
+
+}
 
 
