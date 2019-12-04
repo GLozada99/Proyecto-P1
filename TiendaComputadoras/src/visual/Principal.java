700d5fe6e6a3db93eadca0d1384dccd453356730
gusgo <gusgo@LAPTOP-TPPBSJFQ>
2019-12-04 07:27:49 -0800

    lo de Olyber

--- TiendaComputadoras/src/visual/Principal.java
+++ TiendaComputadoras/src/visual/Principal.java
@@ -34,6 +34,9 @@ import logica.RAM;
 import logica.Tienda;
 import java.awt.Toolkit;
 import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Principal extends JFrame {
 
@@ -133,20 +136,22 @@ public class Principal extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1820, 1000);
 		setLocationRelativeTo(null);
-		setResizable(false);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 
 		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
+		menuBar.setBackground(new Color(86, 130, 163));
+		setJMenuBar(menuBar); 
 
 		JMenu mnAdministracion = new JMenu("Administracion");
+		mnAdministracion.setForeground(new Color(255, 255, 255));
 		mnAdministracion.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/gear_in.png")));
 		mnAdministracion.setEnabled(false);
 		menuBar.add(mnAdministracion);
 
 		JMenuItem mntmAgregarComponentes = new JMenuItem("Agregar Componentes");
+		mntmAgregarComponentes.setForeground(new Color(86, 130, 163));
 		mntmAgregarComponentes.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconAgregarComponente.png")));
 		mntmAgregarComponentes.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -159,6 +164,7 @@ public class Principal extends JFrame {
 
 
 		JMenuItem mntmAgregarProveedores = new JMenuItem("Agregar Proveedores");
+		mntmAgregarProveedores.setForeground(new Color(86, 130, 163));
 		mntmAgregarProveedores.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconAdmin.png")));
 		mntmAgregarProveedores.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -170,6 +176,7 @@ public class Principal extends JFrame {
 		mnAdministracion.add(mntmAgregarProveedores);
 
 		JMenuItem mntmNuevoCombo = new JMenuItem("Nuevo Combo");
+		mntmNuevoCombo.setForeground(new Color(86, 130, 163));
 		mntmNuevoCombo.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconAgregarCombopng.png")));
 		mntmNuevoCombo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -180,6 +187,7 @@ public class Principal extends JFrame {
 		});
 
 		JMenuItem mntmNuevoUsuario = new JMenuItem("Nuevo Usuario");
+		mntmNuevoUsuario.setForeground(new Color(86, 130, 163));
 		mntmNuevoUsuario.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconUsuario.png")));
 		mntmNuevoUsuario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -193,6 +201,7 @@ public class Principal extends JFrame {
 		mnAdministracion.add(mntmNuevoCombo);
 
 		JMenuItem mntmAsignarOrdenDe = new JMenuItem("Asignar orden de compra");
+		mntmAsignarOrdenDe.setForeground(new Color(86, 130, 163));
 		mntmAsignarOrdenDe.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconOrdenesCompra.png")));
 		mnAdministracion.add(mntmAsignarOrdenDe);
 		mntmAsignarOrdenDe.addActionListener(new ActionListener() {
@@ -204,10 +213,12 @@ public class Principal extends JFrame {
 		});
 
 		JMenu mnFacturacion = new JMenu("Facturaci\u00F3n");
+		mnFacturacion.setForeground(new Color(255, 255, 255));
 		mnFacturacion.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconFacturar.png")));
 		menuBar.add(mnFacturacion);
 
 		JMenuItem mntmNuevaFactura = new JMenuItem("Nueva Factura");
+		mntmNuevaFactura.setForeground(new Color(86, 130, 163));
 		mntmNuevaFactura.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconFactura.png")));
 		mntmNuevaFactura.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -219,6 +230,7 @@ public class Principal extends JFrame {
 		mnFacturacion.add(mntmNuevaFactura);
 
 		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Abonar/Saldar Credito Cliente");
+		mntmNewMenuItem_1.setForeground(new Color(86, 130, 163));
 		mntmNewMenuItem_1.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconAbonarSaldo.png")));
 		mntmNewMenuItem_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -230,10 +242,12 @@ public class Principal extends JFrame {
 		mnFacturacion.add(mntmNewMenuItem_1);
 
 		JMenu mnListados = new JMenu("Listados");
+		mnListados.setForeground(new Color(255, 255, 255));
 		mnListados.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconLista.png")));
 		menuBar.add(mnListados);
 
 		JMenuItem mntmListaClientes = new JMenuItem("Lista Clientes");
+		mntmListaClientes.setForeground(new Color(86, 130, 163));
 		mntmListaClientes.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconListaClientes.png")));
 		mntmListaClientes.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -244,6 +258,7 @@ public class Principal extends JFrame {
 		});
 		
 				JMenuItem mntmListaUsuarios = new JMenuItem("Lista Usuarios");
+				mntmListaUsuarios.setForeground(new Color(86, 130, 163));
 				mntmListaUsuarios.setEnabled(false);
 				mntmListaUsuarios.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
@@ -258,6 +273,7 @@ public class Principal extends JFrame {
 		mnListados.add(mntmListaClientes);
 
 		JMenuItem mntmListaProveedores = new JMenuItem("Lista Proveedores");
+		mntmListaProveedores.setForeground(new Color(86, 130, 163));
 		mntmListaProveedores.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconListaProveedor.png")));
 		mntmListaProveedores.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -274,6 +290,7 @@ public class Principal extends JFrame {
 		mnListados.add(mntmListaProveedores);
 
 		JMenuItem mntmListaComponentes = new JMenuItem("Lista Componentes");
+		mntmListaComponentes.setForeground(new Color(86, 130, 163));
 		mntmListaComponentes.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconListaComponentes.png")));
 		mntmListaComponentes.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -285,6 +302,7 @@ public class Principal extends JFrame {
 		mnListados.add(mntmListaComponentes);
 
 		JMenuItem mntmListaCombos = new JMenuItem("Lista Combos");
+		mntmListaCombos.setForeground(new Color(86, 130, 163));
 		mntmListaCombos.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconListaCombos.png")));
 		mntmListaCombos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -296,6 +314,7 @@ public class Principal extends JFrame {
 		mnListados.add(mntmListaCombos);
 
 		JMenuItem mntmHistorialFacturas = new JMenuItem("Historial Facturas");
+		mntmHistorialFacturas.setForeground(new Color(86, 130, 163));
 		mntmHistorialFacturas.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconListaFacturar.png")));
 		mntmHistorialFacturas.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -307,6 +326,7 @@ public class Principal extends JFrame {
 		mnListados.add(mntmHistorialFacturas);
 
 		JMenuItem mntmHistorialOrdenes = new JMenuItem("Historial Ordenes de Compra");
+		mntmHistorialOrdenes.setForeground(new Color(86, 130, 163));
 		mntmHistorialOrdenes.setIcon(new ImageIcon(Principal.class.getResource("/Imagenes/IconListaOrdenCompra.png")));
 		mntmHistorialOrdenes.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -336,5 +356,3 @@ public class Principal extends JFrame {
 		contentPane.add(panel_3);
 	}
 }
-
-
