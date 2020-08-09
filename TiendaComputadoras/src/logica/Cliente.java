a41bd86d4b42cdca2067d4380ad8c89224d2e5fc
GLozada99 <57233171+GLozada99@users.noreply.github.com>
2020-08-08 18:39:26 -0700

    Create Cliente.java

---
+++ TiendaComputadoras/src/logica/Cliente.java
@@ -0,0 +1,43 @@
+package logica;
+
+import java.io.Serializable;
+
+public class Cliente extends Persona implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private float limCredito;
+	private float credito;
+	
+	
+	public Cliente(String nombre, String telefono, String direccion, String cedula) {
+		super(nombre, telefono, direccion, cedula);
+		credito = 0;
+		limCredito = 1000;
+		
+	}
+	
+	public float getCredito() {
+		return credito;
+	}
+	
+	public void setCredito(float credito) {
+		this.credito = credito;
+	}
+	
+	public float getLimCredito() {
+		return limCredito;
+	}
+	
+	public boolean comprobarLimCredito(float credito) {
+		boolean type = false;
+		if(credito+getCredito() > limCredito) {
+			type = true;
+		}
+		
+		return type;
+		
+	}
+	
+}
